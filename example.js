6f56104a1b65e65ad9dfb8e71bc4d9ee80350a00
Auspicus <elijahdisch@gmail.com>
2019-06-30 00:12:27 -0500

    Setup devour-client to pull attributes and relationships from OpenAPI definitions, added example

---
+++ example.js
@@ -0,0 +1,16 @@
+const DevourDrupal = require('devour-drupal')
+
+const getClient = async () => {
+  const client = new DevourDrupal({ baseUrl: 'https://dev-example.pantheonsite.io' })
+  await client.init('https://dev-example.pantheonsite.io/openapi/jsonapi?_format=json')
+  client.addBearer('EXAMPLE_BEARER_TOKEN')
+  return client
+}
+
+const useClient = async () => {
+  const client = await getClient()
+  const { data } = await client.findAll('node--article', { include: 'uid' })
+  console.log(data)
+}
+
+useClient()
--- index.js
+++ index.js
@@ -12,46 +12,122 @@
 
 const JsonApi = require('devour-client')
 
-const jsonapi = new JsonApi({
-  logger: false,
-  apiUrl: 'https://example.com/jsonapi',
-  pluralize: false
-})
+function DevourDrupal({
+  baseUrl,
+  jsonApi = '/jsonapi',
+  logger = true,
+}) {
+  this.baseUrl = baseUrl
+  this.jsonApi = jsonApi
+  this.devour = new JsonApi({
+    logger,
+    apiUrl: `${baseUrl}${jsonApi}`,
+    pluralize: false,
+  })
 
-/**
- * We can do this automatically with access to JSON:API Schema.
- * It's probably best that configuration is cached so that it does
- * not need to be fetched before every initialization of the client.
- * 
- * Models must be defined before you can fetch them. Here's a simple model:
- */
-jsonapi.define('node--article', {
-  nid: '',
-})
+  /**
+   * Devour assumes resources are locatable at their direct path:
+   * 
+   * ie.
+   *  - default devour: /jsonapi/node--article
+   *  - drupal resource: /jsonapi/node/article
+   * 
+   * We have to transform this URL such that it correctly finds the
+   * Drupal resources. This middleware replaces "[entity_type]--[entity_bundle]"
+   * with "[entity_type]/[entity_bundle]" in the URL.
+   */
+  this.devour.insertMiddlewareBefore('axios-request', {
+    name: 'drupalize-url',
+    req: (payload) => {
+      const modelParts = payload.req.model.split('--')
+      payload.req.url = payload.req.url.replace(payload.req.model, `${modelParts[0]}/${modelParts[1]}`)
+      return payload
+    }
+  })
 
-/**
- * Devour assumes resources are locatable at their direct path:
- * 
- * ie.
- *  - default devour: /jsonapi/node--article
- *  - drupal resource: /jsonapi/node/article
- * 
- * We have to transform this URL such that it correctly finds the
- * Drupal resources. This middleware replaces "[entity_type]--[entity_bundle]"
- * with "[entity_type]/[entity_bundle]" in the URL.
- */
-jsonapi.insertMiddlewareBefore('axios-request', {
-  name: 'drupalize-url',
-  req: (payload) => {
-    const modelParts = payload.req.model.split('--')
-    payload.req.url = payload.req.url.replace(payload.req.model, `${modelParts[0]}/${modelParts[1]}`)
-    return payload
+  this.addBearer = function (token) {
+    this.devour.headers['Authorization'] = `Bearer ${token}`
   }
-})
 
-/**
- * Use regular Devour API's to access Drupal data.
- */
-jsonapi.findAll('node--article').then(console.log)
+  this.init = function (openApi) {
+    return this.devour.axios
+      .get(openApi)
+      .then(response => {
+        Object.keys(response.data.definitions)
+          .map(key => {
+            try {
+              const definition = response.data.definitions[key]
+              const attributes = definition.properties.data.properties.attributes
+              const relationships = definition.properties.data.properties.relationships
+              let relationshipProperties = {}
+              try {
+                relationshipProperties = (
+                  Object.keys(relationships.properties)
+                  .map(key => {
+                    const relationship = relationships.properties[key]
+                    const relationshipType = relationship.properties.data.type
+                    let relationshipModel
 
-module.exports.JsonApi = jsonapi
+                    if (relationshipType === 'array') {
+                      relationshipModel = relationship.properties.data.items.properties.type.enum
+                    } else {
+                      relationshipModel = relationship.properties.data.properties.type.enum
+                    }
+
+                    return {
+                      key,
+                      value: {
+                        jsonApi: relationshipType === 'array' ? 'hasMany' : 'hasOne',
+                        type: relationshipModel
+                      }
+                    }
+                  })
+                  .reduce((prev, curr) => {
+                    prev[curr.key] = curr.value
+                    return prev
+                  }, {})
+                )
+              } catch (err) { console.error(err) }
+
+              this.devour.define(key, {
+                ...Object.keys(attributes.properties)
+                      .map(key => ({
+                        key,
+                        value: attributes.properties[key]
+                      }))
+                      .reduce((prev, curr) => {
+                        let type
+                        switch (curr.value.type) {
+                          case 'boolean':
+                            type = false
+                            break
+                          case 'string':
+                            type = ''
+                            break
+                          case 'float':
+                          case 'integer':
+                          case 'number':
+                            type = 0
+                            break
+                          case 'object':
+                            type = {}
+                            break
+                          case 'array':
+                            type = []
+                            break
+                        }
+                        prev[curr.key] = type
+                        return prev
+                      }, {}),
+                ...relationshipProperties,
+              })
+            } catch (err) {
+              console.error(`Failed to define model for ${key}`, err)
+            }
+          })
+      })
+      .then(() => this)
+  }
+}
+
+module.exports.DevourDrupal = DevourDrupal
